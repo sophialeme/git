1a0807bbc3e92cb4c241dc8bf19a13d86d7acd38
Sophia Leme <sophia.lemet1@gmail.com>
2025-04-21 16:01:26 -0300

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,21 @@
-import './App.css'
-import Header from './components/Header'
-import Banner from './components/Banner'
-import ProductList from './components/ProductList'
-import Footer from './components/Footer'
+import React from 'react';
+import './App.css';
+import Header from './components/Header';
+import Banner from './components/Banner';
+import ProductList from './components/ProductList';
+import Footer from './components/Footer';
 
 function App() {
   return (
-    <>
+    <div className="App">
       <Header />
       <div className="container">
         <Banner />
         <ProductList />
       </div>
       <Footer />
-    </>
-  )
+    </div>
+  );
 }
 
-export default App
+export default App;
