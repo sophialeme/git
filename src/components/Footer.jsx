a7566ccb0f95004f009bd4906500b0c0e96938e6
Sophia Leme <sophia.lemet1@gmail.com>
2025-04-22 20:32:52 -0300

    commit

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -17,24 +17,24 @@ const Footer = () => {
       <div className="sociais">
           <div className="img">
               <img className="logo" src="https://img.icons8.com/?size=100&id=118467&format=png&color=FFFFFF"/>
+          </div>
+          <div className="img">
+              <img className="logo" src="https://img.icons8.com/?size=100&id=32309&format=png&color=FFFFFF"/>
+          </div>
+          <div className="img">
+              <img className="logo" src="https://img.icons8.com/?size=100&id=8808&format=png&color=FFFFFF"/>
+          </div>
+          <div className="img">
+              <img className="logo" src="https://img.icons8.com/?size=100&id=aL0F8ToUoPjC&format=png&color=FFFFFF"/>
+          </div>
+        </div>
       </div>
- <div className="img">
-     <img className="logo" src="https://img.icons8.com/?size=100&id=32309&format=png&color=FFFFFF"/>
- </div>
- <div className="img">
-     <img className="logo" src="https://img.icons8.com/?size=100&id=8808&format=png&color=FFFFFF"/>
- </div>
- <div className="img">
-     <img className="logo" src="https://img.icons8.com/?size=100&id=aL0F8ToUoPjC&format=png&color=FFFFFF"/>
-              </div>
-            </div>
-          </div>
-          <div>
-              <ul className="ul2">
-                  <li> 872 Morada Feliz - Cidade das Flores</li>
-                  <li>MG / 39567 - 123</li>
-              </ul>
-          </div>
+      <div>
+          <ul className="ul2">
+              <li> 872 Morada Feliz - Cidade das Flores</li>
+              <li>MG / 39567 - 123</li>
+          </ul>
+      </div>
      </footer>
  </>
   );
